062b004eb444c331f23f633160ea0e7e01679581
AlparSzabados <c3sp3nar@yahoo.com>
2016-01-09 16:28:43 +0200

    Created a test for the RockGameClass.

---
+++ src/test/groovy/szabados/alpar/rockPaperScissors/RockGameClassTest.groovy
@@ -0,0 +1,29 @@
+package szabados.alpar.rockPaperScissors
+
+import spock.lang.Specification
+import static szabados.alpar.rockPaperScissors.RockGameClass.GamePossibilities.*
+import static szabados.alpar.rockPaperScissors.RockGameClass.GameResults.*
+
+/**
+ *  Test for the Rock, Paper, Scissors Game.
+ */
+class RockGameClassTest extends Specification {
+/*@formatter:off*/
+    def 'Rock Paper Scissors Test'() {
+        when:   def rPS = RockGameClass.gameWinner(computerChoice, playerChoice)
+        then:   rPS == winner
+
+        where:  computerChoice | playerChoice || winner
+                PAPER          | ROCK         || COMPUTER_WINS
+                ROCK           | SCISSORS     || COMPUTER_WINS
+                SCISSORS       | PAPER        || COMPUTER_WINS
+                ROCK           | PAPER        || PLAYER_WINS
+                SCISSORS       | ROCK         || PLAYER_WINS
+                PAPER          | SCISSORS     || PLAYER_WINS
+                PAPER          | PAPER        || TIE
+                SCISSORS       | SCISSORS     || TIE
+                ROCK           | ROCK         || TIE
+    }
+    /*@formatter:on*/
+}
+
