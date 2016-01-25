8aa4ccc71c59dccb4616d21bbba3e06d8470b708
AlparSzabados <c3sp3nar@yahoo.com>
2016-01-25 21:00:04 +0200

    All permutations ofa small string in a big string

---
+++ src/main/groovy/szabados/alpar/exercises/permutations.groovy
@@ -0,0 +1,26 @@
+package szabados.alpar.exercises
+
+// TODO write test
+
+/* All permutations of a small string in a big string */
+
+def text = 'abcdefgabcdefgabcdefgabcdefgabcdefgabcdefgabcdefgabcdefgabcdefghijk'
+def word = 'abcdefghijk'
+
+def bigStringUnsorted = []
+def smallStringSorted = word.toList().sort()
+
+def wordEnd = word.size() - 1
+def textSize = text.size() - word.size()
+for (i in 0..textSize) {
+    bigStringUnsorted << text[i..wordEnd]
+    wordEnd += 1
+}
+
+bigStringUnsorted.eachWithIndex { unsorted, index ->
+    def list = unsorted as List
+    def bigStringSorted = list.sort()
+    if (bigStringSorted == smallStringSorted) {
+        println "Permutation on index ${index} : ${unsorted}"
+    }
+}
