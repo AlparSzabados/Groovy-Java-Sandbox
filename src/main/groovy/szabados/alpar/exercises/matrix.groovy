e3bfde9e3c184aaec257f478046f7e1cb9c4df6f
AlparSzabados <c3sp3nar@yahoo.com>
2016-01-25 19:19:33 +0200

    Zero out the row an column of each 0 element in a matrix. Also with test file.

--- src/main/groovy/szabados/alpar/exercises/matrix.groovy
+++ src/main/groovy/szabados/alpar/exercises/matrix.groovy
@@ -2,20 +2,28 @@ package szabados.alpar.exercises
 
 /** Zero out the row an column of each 0 element in a matrix */
 
-// TODO write a method that actually changes the rows and columns
-
-def matrix = [[0, 0, 3], [4, 1, 2]]
-//column: 0  1  2
-//row: 0 [0, 0, 3]
-//row: 1 [4, 1, 2]
-
-matrix.eachWithIndex { row, rowIndex ->
-    row.eachWithIndex { number, columnIndex ->
-        if (number == 0) {
-            println "column: ${columnIndex} row: ${rowIndex}" // prints the location of Zeros in the matrix
+class Matrix {
+    static zeroOutMatrix(matrix) {
+        def matrixColumn = []
+        def matrixRow = []
+        matrix.eachWithIndex { row, rowIndex ->
+            row.eachWithIndex { number, columnIndex ->
+                if (number == 0) {
+                    matrixColumn << columnIndex
+                    matrixRow << rowIndex
+                }
+            }
         }
+
+        for (i in matrix) {
+            i[matrixColumn] = 0
+        }
+
+        for (i in 0..matrix[0].size() - 1) {
+            matrixRow.each { int rowNumber ->
+                matrix[rowNumber][i] = 0
+            }
+        }
+        matrix
     }
 }
-
-
-
