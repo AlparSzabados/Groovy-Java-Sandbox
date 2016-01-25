02613c0c078804e33e5edc02f24b8423b3b55ede
AlparSzabados <c3sp3nar@yahoo.com>
2016-01-25 21:05:52 +0200

    modified file name

---
+++ src/main/groovy/szabados/alpar/exercises/MatrixClass.groovy
@@ -0,0 +1,29 @@
+package szabados.alpar.exercises
+
+/** Zero out the row an column of each 0 element in a matrix */
+
+class Matrix {
+    static zeroOutMatrix(matrix) {
+        def matrixColumn = []
+        def matrixRow = []
+        matrix.eachWithIndex { row, rowIndex ->
+            row.eachWithIndex { number, columnIndex ->
+                if (number == 0) {
+                    matrixColumn << columnIndex
+                    matrixRow << rowIndex
+                }
+            }
+        }
+
+        for (row in matrix) {
+            row[matrixColumn] = 0
+        }
+
+        for (number in 0..(matrix[0].size() - 1)) {
+            matrixRow.each { int rowNumber ->
+                matrix[rowNumber][number] = 0
+            }
+        }
+        matrix
+    }
+}
