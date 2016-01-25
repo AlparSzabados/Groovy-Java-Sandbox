160f00ab1906c3822042c6b7281442d715079bb0
AlparSzabados <c3sp3nar@yahoo.com>
2016-01-25 20:55:38 +0200

    Check if two strings are permutations of each other.

---
+++ src/test/groovy/szabados/alpar/exercises/arePermutationsClassTest.groovy
@@ -0,0 +1,31 @@
+package szabados.alpar.exercises
+
+import spock.lang.Specification
+
+import static szabados.alpar.exercises.ArePermutations.*
+
+/**
+ * Test to see if one string is permutation of another
+ */
+
+class ArePermutationsClassTest extends Specification {
+    /*@formatter:off*/
+    def 'Permutations check'() {
+        when:   def permutations = permutationCheck(strinOne, stringTwo)
+        then:   permutations == result
+
+        where:  strinOne    | stringTwo     ||      result
+                'abc'       | 'abc'         ||      true
+                'a'         | 'b'           ||      false
+                'abc'       | 'cba'         ||      true
+                'bca'       | 'BCA'         ||      true
+                '12azx'     | 'az1x2'       ||      true
+                'Ac1bd'     | 'Ab1cd'       ||      true
+                'Dc1ba'     | 'Ab1cd'       ||      true
+                'Ac1bd'     | 'Ab1cd'       ||      true
+                'DcaE2'     | 'Ab1cd'       ||      false
+                'Dcab2'     | 'Ab1cEd'      ||      false
+                'DcaXb2'    | 'AZb1cd'      ||      false
+    }
+    /*@formatter:on*/
+}
