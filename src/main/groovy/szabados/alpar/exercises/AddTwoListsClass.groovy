77b76f905a689304269bc835d24029f357bbb063
AlparSzabados <c3sp3nar@yahoo.com>
2016-02-19 13:24:47 +0200

    Cleaned up the code, trying to make it more compact

--- src/main/groovy/szabados/alpar/exercises/AddTwoListsClass.groovy
+++ src/main/groovy/szabados/alpar/exercises/AddTwoListsClass.groovy
@@ -2,46 +2,39 @@ package szabados.alpar.exercises
 
 class AddTwoLists {
 
-    static addTwoLists(numList1, numList2) {
-        def listOne = numList1.toString().toList()*.toInteger()
-        def listTwo = numList2.toString().toList()*.toInteger()
+    static addTwoLists(num1, num2) {
+        def listOneReverse = num1.toString().toList().reverse()*.toInteger()
+        def listTwoReverse = num2.toString().toList().reverse()*.toInteger()
 
-        // TODO find a better solution to these two
-        def listOneReverse = listOne.reverse()
-        for (i in listTwo.indices)
-            if (listOneReverse[i] == null)
-                listOneReverse[i] = 0
+        def bigNum = (listOneReverse.size() > listTwoReverse.size()) ? listOneReverse : listTwoReverse
+        def smallNum = (listOneReverse.size() < listTwoReverse.size()) ? listOneReverse : listTwoReverse
+        if (listOneReverse.size() == listTwoReverse.size()) {
+            bigNum = listOneReverse
+            smallNum = listTwoReverse
+        }
 
-        def listTwoReverse = listTwo.reverse()
-        for (i in listOne.indices)
-            if (listTwoReverse[i] == null)
-                listTwoReverse[i] = 0
-
-        // TODO make this part understandable
         def resultNumber = []
-        int difference = 0
-        for (i in 0..listTwoReverse.size() - 1) {
-            def n1 = listOneReverse[i]
-            def n2 = listTwoReverse[i]
-            def result1 = n1 + n2
-            def result2 = n1 + n2 + difference
-            if (n1 + n2 > 9) {
-                def reminder = result1 % 10
-                resultNumber << reminder + difference
-                difference = (result1 - reminder) / 10
-            } else if (n1 + n2 + difference > 9) {
-                def reminder = result2 % 10
-                resultNumber << reminder
-                difference = (result2 - reminder) / 10
+
+        int carry = 0
+        for (i in 0..bigNum.size() - 1) {
+            def sum = sum(smallNum, i, bigNum)
+            def result
+            if ((sum + carry) > 9) {
+                result = (sum + carry) % 10
+                resultNumber << result
+                carry = ((sum + carry) - result) / 10
             } else {
-                resultNumber << result1 + difference
-                difference = 0
+                resultNumber << sum + carry
+                carry = 0
             }
         }
-        if (difference > 0)
-            resultNumber << difference
+        if (carry > 0)
+            resultNumber << carry
 
-        resultNumber.reverse().join('') //TODO find a workaround to the NumberFormatException that occurs when transforming long String to Integer.
+        resultNumber.reverse().join('').toBigInteger()
+    }
+    static sum(ArrayList<Integer> smallNum, int i, ArrayList<Integer> bigNum) {
+        (smallNum[i] != null) ? bigNum[i] + smallNum[i] : bigNum[i]
     }
 }
 
