189004c7509d8ebb47becaf351d03c9a80c0709c
AlparSzabados <c3sp3nar@yahoo.com>
2016-03-09 20:01:58 +0200

    Optimized the code.

--- src/main/groovy/szabados/alpar/exercises/SortArray.groovy
+++ src/main/groovy/szabados/alpar/exercises/SortArray.groovy
@@ -1,14 +1,19 @@
 package szabados.alpar.exercises
 
-/* Sort a random array of values */
+/* Sort an array of values */
 
 def values = [1, 2, 3, 9, 1000, 0, 99, 8, 7, 7, 1, 0, 0]
 
-for (i in values.indices)
-    for (j in values.indices) {
-        if (values[i] < values[j]) {
-            values.swap(i, j)
-        }
-    }
+int valueSize = values.size()
+
+for (i in 0..<valueSize) {
+    int minValue = i
+    for (j in (i + 1)..<valueSize)
+        if (values[j] < values[minValue])
+            minValue = j as int
+
+    if (i != minValue)
+        values.swap(i, minValue)
+}
 
 assert values == [0, 0, 0, 1, 1, 2, 3, 7, 7, 8, 9, 99, 1000]
