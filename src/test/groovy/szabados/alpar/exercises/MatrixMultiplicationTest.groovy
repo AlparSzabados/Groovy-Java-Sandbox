3fd9ee9853cd5877c1e64b63a3eb240501489149
AlparSzabados <c3sp3nar@yahoo.com>
2016-03-09 20:02:28 +0200

    Added a second test

--- src/test/groovy/szabados/alpar/exercises/MatrixMultiplicationTest.groovy
+++ src/test/groovy/szabados/alpar/exercises/MatrixMultiplicationTest.groovy
@@ -10,8 +10,10 @@ class MatrixMultiplicationTest extends Specification {
         when:   def result = MultiplyMatrix(matrixOne, matrixTwo)
         then:   result == expectedResult
 
-        where:  matrixOne                         | matrixTwo                        || expectedResult
+        where:  matrixOne                         | matrixTwo                         || expectedResult
                 [[1, 2, 3], [4, 5, 6], [7, 8, 1]] | [[1, 2, 3], [3, 2, 1], [4, 5, 2]] || [[19, 21, 11], [43, 48, 29], [35, 35, 31]]
+                [[4, 4, 4], [2, 2, 2], [3, 3, 3]] | [[1, 4, 3], [3, 2, 4], [1, 2, 4]] || [[20, 32, 44], [10, 16, 22], [15, 24, 33]]
+
     }
     /* formatter: on */
 }
