9da7c2581f1ddfbcc584b705c2251b85916a3610
AlparSzabados <c3sp3nar@yahoo.com>
2016-03-09 20:04:02 +0200

    Added description

--- src/main/groovy/szabados/alpar/exercises/BinaryOrNot.java
+++ src/main/groovy/szabados/alpar/exercises/BinaryOrNot.java
@@ -2,6 +2,8 @@ package szabados.alpar.exercises;
 
 import java.util.Scanner;
 
+/* Write a program which accepts a number and prints whether it is binary or not. */
+
 public class BinaryOrNot {
     public static void main(String[] args) {
         String num;
