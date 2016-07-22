1e849068b9bc87f7197f719ab2c485206f2c3b73
AlparSzabados <c3sp3nar@yahoo.com>
2016-07-23 01:27:16 +0800

    cosmetic refactoring and simplifying.

--- src/main/groovy/szabados/alpar/exercises/euler/Utils.groovy
+++ src/main/groovy/szabados/alpar/exercises/euler/Utils.groovy
@@ -3,18 +3,12 @@ package szabados.alpar.exercises.euler
 class Utils {
     static final ArrayList<Integer> fibonacci(int limit = 0, int n1 = 0, int n2 = 1) {
         ArrayList<Integer> result = [n1, n2]
-
         int fibNum = 0
-        while (fibNum < limit) {
+        while ((fibNum + n1) < limit) {
             fibNum = n1 + n2
             n1 = n2
             n2 = fibNum
-
-            if (fibNum > limit) {
-                break
-            } else {
-                result += fibNum
-            }
+            result += fibNum
         }
         result
     }
