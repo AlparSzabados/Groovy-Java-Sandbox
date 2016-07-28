09a55171667603c4facb75beb5f155108c3872ff
AlparSzabados <c3sp3nar@yahoo.com>
2016-07-28 19:33:40 +0300

    Added euler 5 problem solution.

---
+++ src/test/groovy/szabados/alpar/exercises/euler/Euler_5.groovy
@@ -0,0 +1,34 @@
+package szabados.alpar.exercises.euler
+
+import org.junit.Test
+
+import static szabados.alpar.exercises.euler.Utils.primeFactorsOf
+
+class Euler_5 {
+
+    /**
+     * <strong>Problem 5: Smallest multiple</strong>
+     * <p>
+     * 2520 is the smallest number that can be divided by each of the numbers from 1
+     * to 10 without any remainder.
+     * <p>
+     * What is the smallest positive number that is evenly divisible by all of the
+     * numbers from 1 to 20?
+     * <p>
+     * See also <a href="https://projecteuler.net/problem=5">projecteuler.net problem 5</a>.
+     */
+    @Test
+    public void shouldSolveProblem5() {
+        assert (smallestPositiveNumberEvenlyDivisibleByAllNumbersFrom1To(10)) == 2_520L
+        assert (smallestPositiveNumberEvenlyDivisibleByAllNumbersFrom1To(20)) == 232_792_560L
+    }
+
+    private static long smallestPositiveNumberEvenlyDivisibleByAllNumbersFrom1To(int max) {
+        def lowestCommonMultiple = 2
+        (2..max).each { int n ->
+            if (lowestCommonMultiple % n != 0)
+                lowestCommonMultiple *= primeFactorsOf(n).min()
+        }
+        lowestCommonMultiple
+    }
+}
