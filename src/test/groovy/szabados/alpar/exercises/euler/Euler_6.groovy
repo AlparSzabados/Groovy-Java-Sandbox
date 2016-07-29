ee946f1f2ce22ddf8525aac6ac39d1f3f82694bd
AlparSzabados <szabadosalpar@gmail.com>
2016-07-29 10:50:49 +0300

    Added solution to euler problem 6.

---
+++ src/test/groovy/szabados/alpar/exercises/euler/Euler_6.groovy
@@ -0,0 +1,46 @@
+package szabados.alpar.exercises.euler
+
+import org.junit.Test
+
+class Euler_6 {
+
+    /**
+     * <strong>Problem 6: Sum square difference</strong>
+     * <p>
+     * The sum of the squares of the first ten natural numbers is,
+     * <p>
+     * 1² + 2² + ... + 10² = 385
+     * <p>
+     * The square of the sum of the first ten natural
+     * numbers is,
+     * <p>
+     * (1 + 2 + ... + 10)² = 55² = 3025
+     * <p>
+     * Hence the difference between the sum of the
+     * squares of the first ten natural numbers and the square of the sum is
+     * <p>
+     * 3025 − 385 = 2640.
+     * <p>
+     * Find the difference between the sum of the squares of the first one hundred
+     * natural numbers and the square of the sum.
+     * <p>
+     * See also <a href="https://projecteuler.net/problem=6">projecteuler.net problem 6</a>.
+     */
+    @Test
+    public void shouldSolveProblem6() {
+        assert (differenceBetweenSumOfTheSquaresAndSquareOfTheSumFrom1UpTo(10)) == 2640L
+        assert (differenceBetweenSumOfTheSquaresAndSquareOfTheSumFrom1UpTo(100)) == 25164150L
+    }
+
+    private static long differenceBetweenSumOfTheSquaresAndSquareOfTheSumFrom1UpTo(int max) {
+        return squareOfSumFrom1UpTo(max) - sumOfSquaresFrom1UpTo(max);
+    }
+
+    static long sumOfSquaresFrom1UpTo(int max) {
+        return ((1..max).inject { result, n -> result + n**2 })
+    }
+
+    static long squareOfSumFrom1UpTo(int max) {
+        return ((1..max).collect().sum()**2)
+    }
+}
