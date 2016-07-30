8854e54abc6b97897be10f346000c3f55adbd3b3
AlparSzabados <c3sp3nar@yahoo.com>
2016-07-30 14:49:58 +0300

    Added `isPrime` method for Euler_7

--- src/test/groovy/szabados/alpar/exercises/euler/Utils.groovy
+++ src/test/groovy/szabados/alpar/exercises/euler/Utils.groovy
@@ -37,4 +37,12 @@ class Utils {
         }
         numReverse
     }
+    static boolean isPrime(long n) {
+        if (n == 2L) return true
+        if (n % 2 == 0L && n != 2L) return false
+        for (long i = 3; i <= Math.sqrt(n) + 1; i += 2) {
+            if (n % i == 0L) return false;
+        }
+        return true;
+    }
 }
