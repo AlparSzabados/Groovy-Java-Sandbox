356cc2ff3450e02dd173e05ba7f569e54df51307
AlparSzabados <c3sp3nar@yahoo.com>
2016-08-10 23:24:25 +0300

    added createMatrix to transform string to 2D matrix for Euler problem 11

--- src/test/groovy/szabados/alpar/exercises/euler/Utils.groovy
+++ src/test/groovy/szabados/alpar/exercises/euler/Utils.groovy
@@ -49,4 +49,25 @@ class Utils {
     static isDivisible(int dividend, int divisor) {
         dividend % divisor == 0
     }
+
+    static createMatrix(String st) {
+        def s = st.replaceAll("\\s", '')
+        def MATRIX = []
+        def rows = []
+        def counter = 0
+
+        for (int i = 0; i < s.length() - 1; i += 2) {
+            def num = ''
+            for (int j = i; j <= i + 1; j++)
+                num += s[j]
+            rows += num
+            counter++
+            if (counter == 20) {
+                MATRIX += [rows]
+                rows = []
+                counter = 0
+            }
+        }
+        MATRIX
+    }
 }
