e51d7ca7838d87546a9340b0ca675aa61a520271
AlparSzabados <c3sp3nar@yahoo.com>
2016-08-12 14:30:54 +0300

    Got rid of typed in loops where they can be derived from context.

--- src/test/groovy/szabados/alpar/exercises/euler/Euler_11.groovy
+++ src/test/groovy/szabados/alpar/exercises/euler/Euler_11.groovy
@@ -1,16 +1,16 @@
 package szabados.alpar.exercises.euler
 
 class Euler_11 {
-    static greatestProductOfAdjacentNumbersInAGrid(List grid, int sizeOfConsecutive) {
-        def maxHorizontal = rightAndDown(grid, sizeOfConsecutive)
-        def maxDiagonalUp = diagonalRight(grid, sizeOfConsecutive)
-        def maxDiagonalDown = diagonalLeft(grid, sizeOfConsecutive)
+    static greatestProductOfAdjacentNumbersInAGrid(List<List<Integer>> grid, int sizeOfConsecutive) {
+        def maxProductOfRightAndDown = rightAndDown(grid, sizeOfConsecutive)
+        def maxProductOfDiagonalRight = diagonalRight(grid, sizeOfConsecutive)
+        def maxProductOfDiagonalLeft = diagonalLeft(grid, sizeOfConsecutive)
 
-        [maxHorizontal, maxDiagonalUp, maxDiagonalDown].max()
+        [maxProductOfRightAndDown, maxProductOfDiagonalRight, maxProductOfDiagonalLeft].max()
     }
 
     static rightAndDown(List<List<Integer>> grid, int sizeOfConsecutive) {
-        def maxHorizontalAndVertical = 0
+        def maxRightAndDown = 0
         for (k in 0..<grid.size()) {
             for (i in 0..(grid.size() - sizeOfConsecutive)) {
                 def horizontal = 1
@@ -19,46 +19,44 @@ class Euler_11 {
                     horizontal *= grid[k][j]
                     vertical *= grid[j][k]
                 }
-                maxHorizontalAndVertical = (horizontal > vertical) ? horizontal : vertical
+                maxRightAndDown = (horizontal > vertical) ? horizontal : vertical
             }
         }
-        maxHorizontalAndVertical
+        maxRightAndDown
     }
 
 
-    static diagonalRight(List<List<Integer>> matrix, int sizeOfConsecutive) {
-        def maxDiagonalUp = 0
-        for (int k in 0..(matrix.size() - sizeOfConsecutive)) {
-            for (int i in 0..(matrix.size() - sizeOfConsecutive)) {
+    static diagonalRight(List<List<Integer>> grid, int sizeOfConsecutive) {
+        def maxDiagonalRight = 0
+        for (k in 0..(grid.size() - sizeOfConsecutive)) {
+            for (i in 0..(grid.size() - sizeOfConsecutive)) {
                 def result = 1
                 def x = 0
                 for (int j = k; j < k + sizeOfConsecutive; j++) {
-                    result *= matrix[j][i + x]
+                    result *= grid[j][i + x]
                     x++
                 }
-                if (result > maxDiagonalUp) {
-                    maxDiagonalUp = result
-                }
+                if (result > maxDiagonalRight)
+                    maxDiagonalRight = result
             }
         }
-        maxDiagonalUp
+        maxDiagonalRight
     }
 
-    static diagonalLeft(List<List<Integer>> matrix, int sizeOfConsecutive) {
-        def maxDiagonalDown = 0
-        for (int k in 0..matrix.size() - sizeOfConsecutive) {
-            for (int i in (sizeOfConsecutive - 1)..<matrix.size()) {
+    static diagonalLeft(List<List<Integer>> grid, int sizeOfConsecutive) {
+        def maxDiagonalLeft = 0
+        for (k in 0..(grid.size() - sizeOfConsecutive)) {
+            for (i in (sizeOfConsecutive - 1)..<grid.size()) {
                 def result = 1
                 def x = 0
                 for (int j = k; j < k + sizeOfConsecutive; j++) {
-                    result *= matrix[j][i + x]
+                    result *= grid[j][i + x]
                     x--
                 }
-                if (result > maxDiagonalDown) {
-                    maxDiagonalDown = result
-                }
+                if (result > maxDiagonalLeft)
+                    maxDiagonalLeft = result
             }
         }
-        maxDiagonalDown
+        maxDiagonalLeft
     }
 }
