362efeef13cd75a06cc75c6f40bb66cb4e88a704
Szabados Alpár <szabadosalpar@gmail.com>
2017-03-15 12:08:21 +0200

    My solution to LeetCode problem nr. 56, `Merge Intervals`

---
+++ src/main/java/szabados/alpar/exercises/MergeIntervals.java
@@ -0,0 +1,53 @@
+package szabados.alpar.exercises;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+
+/* Given a collection of intervals, merge all overlapping intervals.
+ *
+ * For example,
+ * Given [1,3],[2,6],[8,10],[15,18],
+ * return [1,6],[8,10],[15,18].
+ */
+
+public class MergeIntervals {
+    static public List<Interval> merge(List<Interval> intervals) {
+        LinkedList<Interval> result = new LinkedList<>();
+        if (intervals.size() <= 1) return intervals;
+        else {
+            intervals.sort(Comparator.comparing(i -> i.start));
+            for (Interval next : intervals) {
+                if (result.isEmpty()) {
+                    result.add(next);
+                } else if (isOverlapping(result.getLast(), next)) {
+                    Interval curr = result.getLast();
+                    result.getLast().end = (curr.end > next.end ? curr : next).end;
+                } else {
+                    result.add(next);
+                }
+            }
+        }
+        return result;
+    }
+
+    private static boolean isOverlapping(Interval curr, Interval next) {
+        return curr.start <= next.end && curr.end >= next.start;
+    }
+}
+
+class Interval {
+    int start;
+    int end;
+
+    Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+}
