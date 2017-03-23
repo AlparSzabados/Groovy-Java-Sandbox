7b0d7565c86eb65562e364d234bca07da9f50468
Szabados Alpár <szabadosalpar@gmail.com>
2017-03-23 19:30:45 +0200

    My solution to LeetCode problem nr. 398 `Random Pick Index`

--- src/main/java/szabados/alpar/exercises/MergeIntervals.java
+++ src/main/java/szabados/alpar/exercises/MergeIntervals.java
@@ -1,8 +1,6 @@
 package szabados.alpar.exercises;
 
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 
 /* Given a collection of intervals, merge all overlapping intervals.
@@ -13,12 +11,23 @@ import java.util.List;
  */
 
 public class MergeIntervals {
+    public static void main(String[] args) {
+        List<Interval> intervals = Arrays.asList(new Interval(1, 4), new Interval(5, 6), new Interval(5, 6), new Interval(0, 10));
+        final List<Interval> merge = MergeIntervals.merge(intervals);
+        for (Interval interval : merge) {
+//            System.out.println(interval.start);
+//            System.out.println(interval.end);
+        }
+    }
+
     static public List<Interval> merge(List<Interval> intervals) {
         LinkedList<Interval> result = new LinkedList<>();
         if (intervals.size() <= 1) return intervals;
         else {
             intervals.sort(Comparator.comparing(i -> i.start));
+            Queue<Interval> queue = new LinkedList<>(intervals);
             for (Interval next : intervals) {
+                System.out.println(queue.poll().end);
                 if (result.isEmpty()) {
                     result.add(next);
                 } else if (isOverlapping(result.getLast(), next)) {
---
+++ src/main/java/szabados/alpar/exercises/RandomPickIndex.java
@@ -0,0 +1,34 @@
+package szabados.alpar.exercises;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/*
+ Given an array of integers with possible duplicates, randomly output the index of a given target number.
+ You can assume that the given target number must exist in the array.
+
+ int[] nums = new int[] {1,2,3,3,3};
+ Solution solution = new Solution(nums);
+
+ pick(3) should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+
+ pick(1) should return 0. Since in the array only nums[0] is equal to 1.
+*/
+
+public class RandomPickIndex {
+    int[] nums;
+
+    public RandomPickIndex(int[] nums) {
+        this.nums = nums;
+    }
+
+    public int pick(int target) {
+        final Map<Integer, List<Integer>> listMap = IntStream.range(0, nums.length).boxed()
+                                                             .collect(Collectors.groupingBy(i -> nums[i], Collectors.toList()));
+        final List<Integer> integers = listMap.get(target);
+        return integers.get(new Random().nextInt(integers.size()));
+    }
+}
