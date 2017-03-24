fb6333338581a5ecfb53b02a8ca5b04a9c610f13
Szabados Alpár <szabadosalpar@gmail.com>
2017-03-24 19:26:59 +0200

    My solution to LeetCode problem nr. 500 `Keyboard Row`

---
+++ src/main/java/szabados/alpar/exercises/KeyboardRow.java
@@ -0,0 +1,32 @@
+package szabados.alpar.exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a List of words, return the words that can be typed using letters
+ * of alphabet on only one row's of American keyboard.
+ */
+
+public class KeyboardRow {
+    private static String[] keyboardLine = {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
+
+    public String[] findWords(String[] words) {
+        List<String> validWords = new ArrayList<>();
+        for (String word : words) {
+            if (isOneLine(word, keyboardLine[0])) validWords.add(word);
+            else if (isOneLine(word, keyboardLine[1])) validWords.add(word);
+            else if (isOneLine(word, keyboardLine[2])) validWords.add(word);
+        }
+        return validWords.toArray(new String[validWords.size()]);
+    }
+
+    private boolean isOneLine(String word, String keyboardLine) {
+        for (int j = 0; j < word.length(); j++) {
+            if (!keyboardLine.contains(Character.toString(word.charAt(j)).toLowerCase())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
