1ed7e22356df35fb177f70e81f3e4d2423b1e282
Szabados Alpár <szabadosalpar@gmail.com>
2017-03-26 19:32:23 +0300

    My solution to LeetCode problem nr. 113. `Path Sum II`

---
+++ src/main/java/szabados/alpar/exercises/PathSum2.java
@@ -0,0 +1,50 @@
+package szabados.alpar.exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+ * For example:
+ * Given the below binary tree and sum = 22
+ *
+ *        5
+ *       / \
+ *      4   8
+ *     /   / \
+ *    11  13  4
+ *   /  \    / \
+ *  7    2  5   1
+ *
+ * return [[5,4,11,2], [5,8,4,5]]
+ */
+
+public class PathSum2 {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> results = new ArrayList<>();
+        traverse(root, sum, new ArrayList<>(), results);
+        return results;
+    }
+
+    private void traverse(TreeNode root, int sum, List<Integer> result, List<List<Integer>> results) {
+        if (root != null) {
+            result.add(root.val);
+            if (root.left == root.right && sum == root.val) results.add(new ArrayList<>(result));
+            else {
+                traverse(root.left, sum - root.val, result, results);
+                traverse(root.right, sum - root.val, result, results);
+            }
+            result.remove(result.size() - 1);
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
