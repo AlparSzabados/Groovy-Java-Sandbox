6f2f5d00735f5881d700327bf3c2a2c2204b3e80
Szabados Alpár <szabadosalpar@gmail.com>
2017-04-03 12:01:03 +0300

    My solution to LeetCode problem nr. 260. `Single Number III`

---
+++ src/main/java/szabados/alpar/exercises/SingleNumber3.java
@@ -0,0 +1,19 @@
+package szabados.alpar.exercises;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+ * For example:
+ * Given nums = [1, 2, 1, 3, 2, 5], return [3, 5].
+ */
+public class SingleNumber3 {
+    public int[] singleNumber(int[] nums) {
+        final Map<Integer, Long> collect = Arrays.stream(nums).boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        return Arrays.stream(nums).boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                     .entrySet().stream().filter(e -> e.getValue() == 1).mapToInt(Map.Entry::getKey).toArray();
+    }
+}
