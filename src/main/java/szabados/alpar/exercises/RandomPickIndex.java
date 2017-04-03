57eb8e154d7eed07b9b3c1eaabb50b991e669a58
Szabados Alpár <szabadosalpar@gmail.com>
2017-04-03 12:03:47 +0300

    formatted comments

--- src/main/java/szabados/alpar/exercises/RandomPickIndex.java
+++ src/main/java/szabados/alpar/exercises/RandomPickIndex.java
@@ -6,20 +6,19 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-/*
- Given an array of integers with possible duplicates, randomly output the index of a given target number.
- You can assume that the given target number must exist in the array.
-
- int[] nums = new int[] {1,2,3,3,3};
- Solution solution = new Solution(nums);
-
- pick(3) should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
-
- pick(1) should return 0. Since in the array only nums[0] is equal to 1.
+/**
+ * Given an array of integers with possible duplicates, randomly output the index of a given target number.
+ * You can assume that the given target number must exist in the array.
+ *
+ * int[] nums = new int[] {1,2,3,3,3}
+ * Solution solution = new Solution(nums);
+ *
+ * pick(3) should return either index 2, 3, or 4 randomly. Each index should have equal probability of returning.
+ * pick(1) should return 0. Since in the array only nums[0] is equal to 1.
 */
 
 public class RandomPickIndex {
-    int[] nums;
+    private int[] nums;
 
     public RandomPickIndex(int[] nums) {
         this.nums = nums;
