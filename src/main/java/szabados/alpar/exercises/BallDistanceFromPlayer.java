dd9c2c3545c8a35e2033958769a042e92c232d18
AlparSzabados <c3sp3nar@yahoo.com>
2017-05-07 13:00:59 +0300

    removed `main`

--- src/main/java/szabados/alpar/exercises/BallDistanceFromPlayer.java
+++ src/main/java/szabados/alpar/exercises/BallDistanceFromPlayer.java
@@ -9,10 +9,6 @@ public class BallDistanceFromPlayer {
     private static Ball ball = new Ball();
     private static List<Player> players = new ArrayList<>();
 
-    public static void main(String[] args) {
-        new BallDistanceFromPlayer().calculateDistance("15 64 1 1 11 48 1 2 19 43 1 3 21 51 1 4 20 35 1 5 17 61 1 6 39 48 1 7 29 42 1 8 23 51 1 9 51 20 1 10 36 65 1 11 62 40 2 1 107 40 2 2 63 59 2 3 29 63 2 4 16 64 2 5 67 29 2 6 52 24 2 7 19 36 2 8 64 50 2 9 63 40 2 10 24 50 2 11 52 33");
-    }
-
     private static void setup(String s) {
         String[] input = s.split(" ");
         ball.x = Integer.parseInt(input[0]);
